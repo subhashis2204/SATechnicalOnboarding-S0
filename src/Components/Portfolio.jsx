45e2986db3aebae57069cf7b2b5117ed239f2506
Subhashis Paul <76895635+subhashis2204@users.noreply.github.com>
2023-06-20 05:09:45 +0000

    added the changes to portfolio page

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,29 +31,17 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Project Annapurna 🎉",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "A platform connecting both restaurants and NGOs to reduce food wastage and feed the hungry.",
+    url: "https://github.com/subhashis2204/project-annapurna",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Binge Watch",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
-  },
-  {
-    title: "My Resume Site",
-    description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
-  },
+      "Binge Watch is a web application that allows users to search for movies and TV shows and save them to a watchlist.",
+    url: "https://subhashis2204.github.io/BingeWatch/",
+  }
 ];
 
 const Portfolio = () => {
